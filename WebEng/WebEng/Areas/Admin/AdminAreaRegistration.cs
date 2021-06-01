5aadaf42b73718f0bec956d98e08d86fbc09385e
LocNT22 <ngtaloc@gmail.com>
2021-06-01 18:39:42 +0700

    up

--- WebEng/WebEng/Areas/Admin/AdminAreaRegistration.cs
+++ WebEng/WebEng/Areas/Admin/AdminAreaRegistration.cs
@@ -17,7 +17,7 @@ namespace WebEng.Areas.Admin
             context.MapRoute(
                 "Admin_default",
                 "Admin/{controller}/{action}/{id}",
-                new {  action = "Index", controller = "Home", id = UrlParameter.Optional },
+                new {  action = "Index", controller = "QLBaoCao", id = UrlParameter.Optional },
                 new[] { "WebEng.Areas.Admin.Controllers" }
             );
         }
---
+++ WebEng/WebEng/Areas/Admin/Controllers/QLBaoCaoController.cs
@@ -0,0 +1,42 @@
+﻿using Models.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebEng.Areas.Admin.Controllers
+{
+    public class QLBaoCaoController : Controller
+    {
+        // GET: Admin/QLBaoCao
+        [HttpGet]
+        public ActionResult Index()
+        {
+            ViewBag.begig = -1;
+            ViewBag.end = -1;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string date)
+        {
+            DateTime begin = DateTime.Parse(date.Split('-')[0]);
+            DateTime end = DateTime.Parse(date.Split('-')[1]);
+            DateTime now = DateTime.Now.Date;
+            int result = DateTime.Compare(now, end);
+            if(result<0)
+            {
+                return RedirectToAction("Index", "QLBaoCao");
+            }
+            ViewBag.begig = (now - begin).TotalDays *(-1);
+            ViewBag.end = (now - end).TotalDays * (-1);
+
+            WebEngDbContext db = new WebEngDbContext();          
+
+            List<int> solieu = null;
+            solieu[0] = db.BinhLuans.Count()+db.TaiKhoans.Count()+db.LopHocs.Count()+db.TaiLieux.Count()+db.LichSuGDs.Count()+db.DSLopHocs.Count();
+            ViewBag.thongkeDau=0;
+            return View();
+        }
+    }
+}
